fe0222ee2e2c69c9bc550dfbf3f01696ce6e14a3
Ratmir Aitov <ratmirslv@gmail.com>
2021-11-22 00:37:02 +0300

    fix script

--- youtubeEpisodeEnable.js
+++ youtubeEpisodeEnable.js
@@ -8,12 +8,14 @@
 // @include     https://www.youtube.com/*
 // @match       https://www.youtube.com/*
 // @exclude     https://www.youtube.com
-// @version     0.2
+// @version     0.3
 // @grant       none
 // @homepageURL https://greasyfork.org/ru/scripts/414771
-// @description Отображает список эпизодов справа от видео
 // ==/UserScript==
 
-window.addEventListener("yt-page-data-updated", function () {
-  setTimeout(() => document.querySelector(".ytp-chapter-title").click(), 0);
+//when you open new page - event yt-page-data-updated stopped working
+setTimeout(()=>document.querySelector('.ytp-chapter-title').click(),0)
+
+window.addEventListener('yt-page-data-updated', function () {
+    setTimeout(()=>document.querySelector('.ytp-chapter-title').click(),0)
 });
